72b7e172429349af179bb3596cd14404dec554ed
coderasm <coderasm84k@gmail.com>
2015-09-30 18:39:22 -0700

    Was missing these files.

---
+++ ABSBuybackMVCWebAPI.Tests/Repositories/SaleLocationRepositoryTest.cs
@@ -0,0 +1,24 @@
+﻿using ABSBuybackMVCWebAPI.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ABSBuybackMVCWebAPI.Tests.Repositories
+{
+    [TestClass]
+    public class SaleLocationRepositoryTest
+    {
+        private ISaleLocationRepository repository;
+        
+        [TestInitialize]
+        public void Setup()
+        {
+            repository = new SaleLocationRepository();
+        }
+
+        [TestMethod]
+        public void SaleLocationObjectsContainSaleInstanceIdLists()
+        {
+            var saleLocations = repository.GetAll();
+            Assert.AreEqual(saleLocations.Count, 9);
+        }
+    }
+}
---
+++ ABSBuybackMVCWebAPI/Models/SaleInstance.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Dapper.Contrib.Extensions;
+
+namespace ABSBuybackMVCWebAPI.Models
+{
+    [Table("GroupSaleInstance")]
+    public class SaleInstance
+    {
+        [Key]
+        public int SaleInstanceId { get; set; }
+        public DateTime? SaleFirstDate { get; set; }
+        public DateTime? SaleEndDate { get; set; }
+    }
+}

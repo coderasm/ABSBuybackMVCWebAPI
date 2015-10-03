0c1d1535cdaa2fd8cadf4c7a37b3d3cf87c4eafc
coderasm <coderasm84k@gmail.com>
2015-10-02 21:21:22 -0700

    Adding missed files

---
+++ ABSBuybackMVCWebAPI/Controllers/SaleLocationController.cs
@@ -0,0 +1,45 @@
+﻿using ABSBuybackMVCWebAPI.Models;
+using ABSBuybackMVCWebAPI.Services.Repository;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace ABSBuybackMVCWebAPI.Controllers
+{
+    [RoutePrefix("api/salelocation")]
+    public class SaleLocationController : ApiController
+    {
+        private readonly IRepositoryService repositoryService;
+
+        public SaleLocationController(IRepositoryService repositoryService)
+        {
+            this.repositoryService = repositoryService;
+        }
+
+        // GET: api/SaleLocation
+        public IEnumerable<Location> Get()
+        {
+            return repositoryService.LocationRepository.GetAll();
+        }
+
+        // GET: api/SaleLocation/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/SaleLocation
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/SaleLocation/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/SaleLocation/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ABSBuybackMVCWebAPI/Scripts/app/src/repositories/SaleLocationRepository.js
@@ -0,0 +1,20 @@
+﻿
+import {HttpClient} from 'aurelia-fetch-client';
+import 'fetch';
+
+export class SaleLocationRepository
+{
+    constructor() {
+        this.http = new HttpClient();
+        this.http.configure(config => {
+            config
+              .useStandardConfiguration()
+              .withBaseUrl('http://localhost:63778/api/salelocation/')
+        });
+    }
+
+    getAll()
+    {
+        return this.http.fetch('');
+    }
+}
